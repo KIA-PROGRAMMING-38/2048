ae47a4e75ba4196c9a740a99dc04fd608b9d598d
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-06-14 17:30:23 +0900

    git ignore 및 이슈 템플릿 추

---
+++ 2048/Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
