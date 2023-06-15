6b27edcf1fc09cd4cf4a8ce5831b24588e0da2bd
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-06-15 20:15:31 +0900

    게임 시작 시 랜덤한곳에 2개의 타일 생성 (#1)

--- 2048/Assets/Script/TileBoard.cs
+++ 2048/Assets/Script/TileBoard.cs
@@ -1,18 +1,42 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Tile tilePrefab;
+	public TileState[] tileStates;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private TileGrid grid;
+
+	private List<Tile> tiles;
+
+
+	private void Awake()
+	{
+		grid = GetComponentInChildren<TileGrid>();
+		tiles = new List<Tile>(16);
+	}
+
+	private void Start()
+	{
+		CreateTile();
+		CreateTile();
+	}
+
+	private void CreateTile()
+	{
+		Tile tile = Instantiate(tilePrefab, grid.transform);
+		int randomValue = Random.Range(0, 10);
+
+		if (randomValue > 1)
+		{
+			tile.SetState(tileStates[0], 2);
+		}
+		else
+		{
+			tile.SetState(tileStates[1], 4);
+		}
+		tile.Spawn(grid.GetRandomEmptyCell());
+		tiles.Add(tile);
+	}	
 }
