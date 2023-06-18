56823f26ff86dfa23c46eb0d973335136c113343
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-06-18 20:56:31 +0900

    타일 이동 구현

--- 2048/Assets/Script/TileBoard.cs
+++ 2048/Assets/Script/TileBoard.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,11 +11,13 @@ public class TileBoard : MonoBehaviour
 
 	private List<Tile> tiles;
 
+	private bool delay;
+
 
 	private void Awake()
 	{
 		grid = GetComponentInChildren<TileGrid>();
-		tiles = new List<Tile>(16);
+		tiles = new List<Tile>();
 	}
 
 	private void Start()
@@ -38,5 +41,89 @@ public class TileBoard : MonoBehaviour
 		}
 		tile.Spawn(grid.GetRandomEmptyCell());
 		tiles.Add(tile);
+	}
+
+	private void Update()
+	{
+		if (!delay)
+		{
+			if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				MoveTiles(Vector2Int.up, 0, 1, 1, 1);
+			}
+			else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
+			}
+			else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				MoveTiles(Vector2Int.left, 1, 1, 0, 1);
+			}
+			else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+			}
+		}
+	}
+
+	private void MoveTiles(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
+	{
+		bool changed = false;
+		for (int x = startX; x >= 0 && x < grid.width; x += incrementX)
+		{
+			for (int y = startY; y >= 0 && y < grid.height; y += incrementY)
+			{
+				TileCell cell = grid.GetCell(x, y);
+
+				if (cell.occupied)
+				{
+					changed |= MoveTile(cell.tile, direction);
+				}
+			}
+		}
+
+		if (changed)
+		{
+			StartCoroutine(WaitForChanges());
+		}
+	}
+
+	private bool MoveTile(Tile tile, Vector2Int direction)
+	{
+		TileCell newCell = null;
+		TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);
+
+		while (adjacent != null)
+		{
+			if (adjacent.occupied)
+			{
+				// 합치기 추가 
+				break;
+			}
+
+			newCell = adjacent;
+			adjacent = grid.GetAdjacentCell(adjacent, direction);
+		}
+
+		if (newCell != null)
+		{
+			tile.MoveTo(newCell);
+			return true;
+		}
+
+		return false;
+	}
+
+	private IEnumerator WaitForChanges()
+	{
+		delay = true;
+
+		yield return new WaitForSeconds(0.1f);
+
+		delay = false;
+
+		// 새로운 타일 생성 코드
+
+		// 게임오버 판별 코드
 	}	
 }
