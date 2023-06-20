edd5eed3144cbf2fbd9e1ea03c99f92bd6b561c7
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-06-20 17:26:53 +0900

    점수 추가 (#11)

--- 2048/Assets/Script/TileBoard.cs
+++ 2048/Assets/Script/TileBoard.cs
@@ -142,6 +142,8 @@ public class TileBoard : MonoBehaviour
 		int number = b.number * 2;
 
 		b.SetState(tileStates[index], number);
+
+		gameManager.PlusScore(number);
 	}
 
 	private int IndexOf(TileState state)
