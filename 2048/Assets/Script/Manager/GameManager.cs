3f6eef9ccb81bb8005d4d71f5f03432de34c62bd
Qyubee99 <101865591+Qyubee99@users.noreply.github.com>
2023-06-20 13:58:41 +0900

    게임 오버 구현

--- 2048/Assets/Script/Manager/GameManager.cs
+++ 2048/Assets/Script/Manager/GameManager.cs
@@ -1,16 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+	public TileBoard board;
 
-    void Update()
-    {
-        
-    }
+	public CanvasGroup canvas;
+
+	private void Start()
+	{
+		NewGame();
+	}
+
+	public void NewGame()
+	{
+		canvas.alpha = 0f;
+		canvas.interactable = false;
+
+		board.ClearBoard();
+		board.CreateTile();
+		board.CreateTile();
+		board.enabled = true;
+	}
+
+	public void GameOver()
+	{
+		board.enabled = false;
+		canvas.interactable = true;
+
+		StartCoroutine(Fade(canvas, 1f, 1f));
+	}
+
+	private IEnumerator Fade(CanvasGroup canvasGroup, float to, float delay)
+	{
+		yield return new WaitForSeconds(delay);
+
+		float elapsedTime = 0f;
+		float duration = 0.5f;
+		float from = canvasGroup.alpha;
+
+		while (elapsedTime < duration)
+		{
+			canvasGroup.alpha = Mathf.Lerp(from, to, elapsedTime / duration);
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+
+		canvasGroup.alpha = to;
+	}
 }
